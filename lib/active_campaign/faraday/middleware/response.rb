e0fbf8a1de1a75d157c20b12533dc8f65d9a495d
kenta-s <knt01222@gmail.com>
2021-07-01 13:46:35 +0900

    disable debug

--- lib/active_campaign/faraday/middleware/response.rb
+++ lib/active_campaign/faraday/middleware/response.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'logger'
-require 'faraday/logging/formatter'
 
 module ActiveCampaign
   LOGGER = ::Logger.new(STDOUT)
@@ -21,7 +20,6 @@ module ActiveCampaign
         # Calls the `parse` method if defined
         def on_complete(env)
           env.body = parse(env.body)
-          debug(env) if ActiveCampaign.config.debug?
         end
 
         def parse(body)
@@ -30,21 +28,7 @@ module ActiveCampaign
           body = ::Oj.load(body, mode: :compat)
           transform_keys(body, :underscore)
         end
-
-        def debug(env)
-          formatter.request(env)
-          formatter.response(env)
-        end
-
-        def formatter
-          @formatter ||= ::Faraday::Logging::Formatter.new(
-            logger: LOGGER,
-            options: { headers: true, bodies: true }
-          )
-        end
       end
     end
   end
 end
-
-Faraday::Response.register_middleware active_campaign: ActiveCampaign::Faraday::Middleware::Response
